ec6eb5e788dd813f82cb72b8615619a235210d22
rekvizitt <chekaviyvlad@gmail.com>
2024-10-14 17:44:57 +0300

    Initial

---
+++ SolutionData.cs
@@ -0,0 +1,11 @@
+﻿namespace GKH
+{
+    public class SolutionData
+    {
+        public string Algorithm { get; set; }
+        public int[] Solution { get; set; }
+        public int[] Costs { get; set; }
+        public int TotalSum { get; set; }
+        public long ElapsedMilliseconds { get; set; }
+    }
+}
