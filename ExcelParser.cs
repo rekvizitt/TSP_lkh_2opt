08db5b6e64b83af1826d5ae1925db08d4e3aee88
rekvizitt <chekaviyvlad@gmail.com>
2024-10-18 21:51:41 +0300

    Small fix ExcelParser.cs

--- ExcelParser.cs
+++ ExcelParser.cs
@@ -20,15 +20,17 @@ namespace GKH
         {
             var worksheet = ExcelManager.Read(Globals.SelectedWorksheet);
 
-            // get all cells in a range from matrix start to matrix end = matrix start + matrix size
-            // library uses column names instead of numbers so we use conversion method
-            // if you wanna use column names you'll still need conversion to determine matrix end so why bother
             var cells = worksheet.GetCellsInRange(
                 new CellRange(
                     ConvertColumnNumberToLetter(worksheet, Globals.MatrixY),
                     ConvertColumnNumberToLetter(worksheet, Globals.MatrixY + Globals.MatrixSize - 1),
                     Globals.MatrixX,
                     Globals.MatrixX + Globals.MatrixSize - 1)).ToArray();
+
+            if (cells.Length < Globals.MatrixSize * Globals.MatrixSize)
+            {
+                throw new InvalidOperationException("Not enough cells to fill the distance matrix.");
+            }
 
             var distances = new int[Globals.MatrixSize][];
 
@@ -37,12 +39,22 @@ namespace GKH
                 distances[i] = new int[Globals.MatrixSize];
                 for (var j = 0; j < Globals.MatrixSize; j++)
                 {
-                    distances[i][j] = int.Parse(cells[i * Globals.MatrixSize + j].Value.ToString()!);
+                    var cellValue = cells[i * Globals.MatrixSize + j].Value;
+                    if (cellValue == null)
+                    {
+                        throw new InvalidOperationException($"Cell value at [{i}, {j}] is null.");
+                    }
+
+                    if (!int.TryParse(cellValue.ToString(), out distances[i][j]))
+                    {
+                        throw new FormatException($"Cell value at [{i}, {j}] is not a valid integer.");
+                    }
                 }
             }
 
             return distances;
         }
+
 
         // get cells in first row, get cell with given column number, get column name
         private static string ConvertColumnNumberToLetter(Worksheet worksheet, int columnNumber)
